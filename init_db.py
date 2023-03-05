08e47473fc8b37dc5277ca79c5c54272c40a885a
claunicole <claunicole.gf@gmail.com>
2023-03-05 00:13:42 -0300

    Add data to categories and countries

--- init_db.py
+++ init_db.py
@@ -15,11 +15,35 @@ cur = conn.cursor()
 # Insert data into the table rol
 cur.execute("INSERT INTO ROLES(NAME,STATUS) VALUES ('admin',true)")
 
+# Insert data into the table paises
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Argentina', 'ARG', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Bolivia', 'BOL', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Chile', 'CHL', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Colombia', 'COL', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Costa Rica', 'CRI', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Ecuador', 'ECU', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('México', 'MEX', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Paraguay', 'PRY', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Panamá', 'PAN', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Perú', 'PER', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Puerto Rico', 'PRI', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('República Dominicana', 'DOM', true)")
+cur.execute("INSERT INTO PAISES(NAME, CODE ,STATUS) VALUES ('Uruguay', 'URY', true)")
+
+# Insert data into the table categorias
+cur.execute("INSERT INTO CATEGORIAS(NAME,STATUS) VALUES ('Seguridad',true)")
+cur.execute("INSERT INTO CATEGORIAS(NAME,STATUS) VALUES ('Transporte',true)")
+cur.execute("INSERT INTO CATEGORIAS(NAME,STATUS) VALUES ('Hospedaje',true)")
+cur.execute("INSERT INTO CATEGORIAS(NAME,STATUS) VALUES ('Turismo ',true)")
+cur.execute("INSERT INTO CATEGORIAS(NAME,STATUS) VALUES ('Clima ',true)")
+cur.execute("INSERT INTO CATEGORIAS(NAME,STATUS) VALUES ('Comida',true)")
+
+
 # Insert data into the table user
 cur.execute('INSERT INTO USERS (USERNAME,EMAIL,PASSWORD,ROL_ID,STATUS)'
             'VALUES (%s, %s, %s, %s,%s)',
             ('superadmin',
-             'superadmin@gmail',
+             'superadmin@gmail.com',
              generate_password_hash('12345678',method="sha256"),
              1,
              True)
